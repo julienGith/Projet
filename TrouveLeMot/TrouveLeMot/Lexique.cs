8d3aeb37236314d0b5a872cdf0722308b9f796b7
julienGith <53513531+julienGith@users.noreply.github.com>
2019-08-05 00:51:55 +0200

    Lexique ver1

--- TrouveLeMot/TrouveLeMot/Lexique.cs
+++ TrouveLeMot/TrouveLeMot/Lexique.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace TrouveLeMot
 {
-    class Lexique
+    class Lexique : List<string>
     {
+
+        Lexique lexique = new Lexique();
+
+        public string mot;
+
+        public new void Add(string mot)
+        {
+            bool trouve = false;
+             if (lexique.Contains(mot))
+             {
+                trouve = true;
+             }
+            if (!trouve)
+            {
+                base.Add(mot);
+            }
+        }
+        public new void Remove(string mot)
+        {
+            bool trouve = false;
+            if (lexique.Contains(mot))
+            {
+                trouve = true;
+            }
+            if (!trouve)
+            {
+                base.Remove(mot);
+            }
+        }
+        public new void Pioche()
+        {
+            Random rand = new Random();
+            int aléatoire = 0;
+            aléatoire = rand.Next(0, lexique.Count-1);
+            base.Add(lexique[aléatoire]);
+        }
     }
 }
