1702be036863e641738e1d084e9890f5e3b9b65f
julienGith <53513531+julienGith@users.noreply.github.com>
2019-08-05 13:08:28 +0200

    Lexique v1 + enregistrement

--- TrouveLeMot/TrouveLeMot/Lexique.cs
+++ TrouveLeMot/TrouveLeMot/Lexique.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TrouveLeMot
 {
-    class Lexique : List<string>
+    [Serializable()]
+   public class Lexique : List<string>
     {
-
+        public Lexique(){ }
         Lexique lexique = new Lexique();
 
         public string mot;
@@ -16,10 +18,10 @@ namespace TrouveLeMot
         public new void Add(string mot)
         {
             bool trouve = false;
-             if (lexique.Contains(mot))
-             {
+            if (lexique.Contains(mot))
+            {
                 trouve = true;
-             }
+            }
             if (!trouve)
             {
                 base.Add(mot);
@@ -41,8 +43,27 @@ namespace TrouveLeMot
         {
             Random rand = new Random();
             int aléatoire = 0;
-            aléatoire = rand.Next(0, lexique.Count-1);
+            aléatoire = rand.Next(0, lexique.Count - 1);
             base.Add(lexique[aléatoire]);
         }
+        #region 
+        /// <summary>
+        /// Methodes de sauvegarde/chargement du Lexique.
+        /// </summary>
+        /// <param name="path"></param>
+        public void SaveXML(string path)
+        {
+            System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(Salaries));
+            System.IO.FileStream file = System.IO.File.Create(path);
+            writer.Serialize(file, this);
+            file.Close();
+        }
+        public void LoadXML(string path)
+        {
+            System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(Salaries));
+            FileStream file = new FileStream(path, FileMode.Open);
+            serializer.Deserialize(file);
+        }
+        #endregion
     }
 }
